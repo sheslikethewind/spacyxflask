adf5303849c4ab2b17076abad09cf030b53cc7ee
CoLoDot <constancelouise.gauriau@gmail.com>
2020-06-23 23:26:22 +0200

    :recycle: Add map to UI

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from flask import Flask, render_template, request
 from utils.spaCy_analysis import spaCy_analysis
+from utils.coordonates import get_nominatim_coordonates
 
 app = Flask(__name__)
 
@@ -13,8 +14,9 @@ def response():
     try:
         if request.method == 'POST':
             form_input = request.form.get('search')
-            response = spaCy_analysis(string_to_analyse=form_input)
-            return render_template('index.html', spaCyResponse=response)
+            cities = spaCy_analysis(string_to_analyse=form_input)
+            coordonates = get_nominatim_coordonates(entities=cities)
+            return render_template('index.html', coordonates=coordonates, cities=str(len(cities)) + ' places found.')
         else:
             return render_template('index.html', spaCyResponse="Error: Please try again")
     except:
--- utils/spaCy_analysis.py
+++ utils/spaCy_analysis.py
@@ -9,4 +9,4 @@ nlp = spacy.load('fr_core_news_sm')
 def spaCy_analysis(string_to_analyse: str, article_getter=get_maitron_article) -> set:
     maitron = article_getter(id=ID)
     treat = nlp(maitron.get('article_intro') + maitron.get('article_content'))
-    return set([t.text for t in treat.ents if t.label_ == 'LOC'])
+    return list(set([t.text for t in treat.ents if t.label_ == 'LOC']))
